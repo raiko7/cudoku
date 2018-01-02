d96b7d00937120edea83c52824e6372da4ca124a
Rainer Koppler <rainer.koppler@gmail.com>
2018-01-02 20:08:56 +0100

    add initial file versions

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# cudoku
-simple Sudoku solver in C, based on backtracking search
+# Overview
+*cudoku* is a simple Sudoku solver based on backtracking search. It is written in plain ANSI C and does not use explicit recursion, so it is blazingly fast (despite the brute-force strategy). Originally *cudoku* was a coding kata idea, created while spending about an hour in a doctor's waiting room.
+
+# Usage
+*cudoku* must be run from the command line. Example:
+```
+cudoku puzzle1.txt
+```
+The argument `puzzle1.txt` is the name of a text file that contains the initial sudoku puzzle, given as a 9 x 9 matrix. For example, the puzzle
+
+| A | B  | C | D | E | F | G | H | I |
+|---|----|---|---|---|---|---|---|---|
+|   |    | 2 | 4 |   | 8 | 6 |   |   |
+|   | 5  |   |   |   |   |   | 1 |   |
+| 9 |    |   |   |   |   |   |   | 7 |
+| 6 |    |   | 9 |   | 7 |   |   | 5 |
+|   |    |   |   |   |   |   |   |   |
+| 2 |    |   | 8 |   | 6 |   |   | 9 |
+| 5 |    |   |   |   |   |   |   | 6 |
+|   | 1  |   |   |   |   |   | 4 |   |
+|   |    | 7 | 5 |   | 2 | 1 |   |   |
+
+must be specified in the following form (empty cells are specified using zeroes):
+```
+1 2 0 0 4 7 0 0 5
+0 3 8 0 0 0 7 0 1
+0 0 0 1 0 3 0 0 9
+8 0 7 4 3 0 6 0 0
+0 1 2 0 0 0 0 9 7
+0 0 4 7 0 2 5 0 3
+0 0 5 0 7 0 1 2 0
+7 6 0 9 0 0 0 0 8
+2 0 3 8 0 1 9 7 0
+```
+
+After solving the puzzle, *cudoku* prints the solution to the standard output:
+```
+1 2 9 6 4 7 8 3 5
+4 3 8 2 9 5 7 6 1
+5 7 6 1 8 3 2 4 9
+8 5 7 4 3 9 6 1 2
+3 1 2 5 6 8 4 9 7
+6 9 4 7 1 2 5 8 3
+9 8 5 3 7 6 1 2 4
+7 6 1 9 2 4 3 5 8
+2 4 3 8 5 1 9 7 6
+```
---
+++ cudoku.c
@@ -0,0 +1,263 @@
+/*
+**    $VER: cudoku.c 1.0 (29.03.06)
+**
+**    simple Sudoku solver, uses backtracking search
+**
+**    (C) Copyright 2006-2017 by Rainer Koppler
+**
+**==============================================================================
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/*------------------------------------------------------------------------------
+*  constants
+*/
+enum {
+   cMinNum   = 1,
+   cMaxNum   = 9,
+   cNumCnt   = cMaxNum - cMinNum + 1,
+   cHistSize = cNumCnt * cNumCnt
+};
+
+static const int csPos2Sqr[cNumCnt][cNumCnt] = {
+   { 0, 0, 0, 1, 1, 1, 2, 2, 2 },
+   { 0, 0, 0, 1, 1, 1, 2, 2, 2 },
+   { 0, 0, 0, 1, 1, 1, 2, 2, 2 },
+   { 3, 3, 3, 4, 4, 4, 5, 5, 5 },
+   { 3, 3, 3, 4, 4, 4, 5, 5, 5 },
+   { 3, 3, 3, 4, 4, 4, 5, 5, 5 },
+   { 6, 6, 6, 7, 7, 7, 8, 8, 8 },
+   { 6, 6, 6, 7, 7, 7, 8, 8, 8 },
+   { 6, 6, 6, 7, 7, 7, 8, 8, 8 }
+};
+
+/*------------------------------------------------------------------------------
+*  types
+*/
+#define true  1
+#define false 0
+
+typedef char     bool;
+typedef int      Number;
+typedef unsigned BSet;
+
+typedef struct {    /* position history */
+   int   mRow;      /* position row */
+   int   mCol;      /* position column */
+   BSet  mAlts;     /* alternatives */
+   int   mAltIdx;   /* current alternative */
+   int   mAltCnt;   /* total number of alternatives */
+} History;
+
+/*------------------------------------------------------------------------------
+*  variables
+*/
+static BSet sRows[cNumCnt];   /* available numbers for each row */
+static BSet sCols[cNumCnt];   /* available numbers for each column */
+static BSet sSqrs[cNumCnt];   /* available numbers for each square */
+
+static Number sMatrix[cNumCnt][cNumCnt];
+static bool   sPreset[cNumCnt][cNumCnt];
+
+static History sHistory[cHistSize];
+
+static int sPresetCnt;
+static int sTotalCnt;
+static int sCnt;
+
+/*------------------------------------------------------------------------------
+*  macros & functions
+*/
+#define BSetAdd(bset, num)      ((bset) |= (1 << (num)))
+#define BSetRemove(bset, num)   ((bset) &= ~(1 << (num)))
+#define BSetIsEmpty(bset)       ((bset) == 0)
+
+/******************************************************************************/
+static int BSetGetLength(BSet bset) {
+
+   int n = 0;
+   while (bset != 0) {
+      if ((bset & 1) != 0) ++n;
+      bset >>= 1;
+   }
+   return n;
+}
+
+/******************************************************************************/
+static int BSetGetNth(BSet bset, int idx) {
+
+   int i;
+   for (i=0; i<cNumCnt && idx>=0; ++i) {
+      if ((bset & 1) != 0 && --idx < 0) return i;
+      bset >>= 1;
+   }
+   return -1;
+}
+
+/******************************************************************************/
+static bool GetFreePos(int *pRow, int *pCol) {
+
+   static bool sFirst = true;
+
+	if (sFirst) {
+		sFirst = false;
+		*pRow = 0;
+		*pCol = 0;
+		if (!sPreset[*pRow][*pCol]) return true;
+	}
+
+	do {
+		++*pCol;
+		if (*pCol == cNumCnt) {
+			*pCol = 0;
+			++*pRow;
+			assert(*pRow != cNumCnt);
+		}
+	} while (sPreset[*pRow][*pCol]);
+
+	return true;
+}
+
+/******************************************************************************/
+static void ExitUnsolved(void) {
+
+	puts("puzzle cannot be solved !");
+	exit(EXIT_FAILURE);
+}
+
+/*------------------------------------------------------------------------------
+*  main
+*/
+int main(int argc, char *argv[]) {
+
+   FILE    *pFile;
+   int      i, j, v, row, col, sqr, altCnt, backtrackCnt = 0;
+	BSet     alts;
+	bool     ok;
+	History *p;
+
+   if (argc < 2) {
+      printf("Usage: %s <matrix file>\n", argv[0]);
+      exit(EXIT_FAILURE);
+   }
+
+   puts("### Brute-force Sudoku Solver ###\nCopyright 2006-2017 by Rainer Koppler\n");
+
+   /*---------------------------------------------------------------------------
+   *  read incomplete matrix
+   */
+   pFile = fopen(argv[1], "r");
+   if (pFile == 0) {
+      printf("Error: could not read \"%s\"\n", argv[1]);
+      exit(EXIT_FAILURE);
+   }
+
+   for (i=0; i<cMaxNum; ++i) {
+
+      fscanf(pFile, "%d %d %d %d %d %d %d %d %d\n",
+         &sMatrix[i][0],
+         &sMatrix[i][1],
+         &sMatrix[i][2],
+         &sMatrix[i][3],
+         &sMatrix[i][4],
+         &sMatrix[i][5],
+         &sMatrix[i][6],
+         &sMatrix[i][7],
+         &sMatrix[i][8]);
+
+		sRows[i] = sCols[i] = sSqrs[i] = (1 << cMaxNum) - 1;   /* start with ALL numbers */
+      for (j=0; j<cMaxNum; ++j) {
+         if (sMatrix[i][j] != 0) {
+				sPreset[i][j] = true;
+				BSetRemove(sRows[i], sMatrix[i][j] - 1);
+				++sPresetCnt;
+			}
+      }
+   }
+
+   fclose(pFile);
+
+   for (j=0; j<cMaxNum; ++j) {
+      for (i=0; i<cMaxNum; ++i) {
+			v = sMatrix[i][j];
+         if (v != 0) {
+				BSetRemove(sCols[j], v - 1);
+				BSetRemove(sSqrs[csPos2Sqr[i][j]], v - 1);
+			}
+      }
+   }
+
+   /*---------------------------------------------------------------------------
+   *  compute matrix elements
+   */
+	sTotalCnt = cNumCnt * cNumCnt - sPresetCnt;
+	sCnt = 0;
+
+	while (sCnt != sTotalCnt) {
+
+		if (!GetFreePos(&row, &col)) ExitUnsolved();
+		sqr = csPos2Sqr[row][col];
+
+		alts = sRows[row] & sCols[col] & sSqrs[sqr];
+		altCnt = BSetGetLength(alts);
+		i = 0;
+		ok = !BSetIsEmpty(alts);
+
+		while (!ok) {
+			if (sCnt == 0) ExitUnsolved();
+			++backtrackCnt;
+
+			--sCnt;
+			p = &sHistory[sCnt];
+			row = p->mRow;
+			col = p->mCol;
+			sqr = csPos2Sqr[row][col];
+
+			v = sMatrix[row][col] - 1;
+			sMatrix[row][col] = 0;
+			BSetAdd(sRows[row], v);
+			BSetAdd(sCols[col], v);
+			BSetAdd(sSqrs[sqr], v);
+
+			alts   = p->mAlts;
+			altCnt = p->mAltCnt;
+			i      = p->mAltIdx;
+			++i;
+			ok = i < p->mAltCnt;
+		}
+
+		v = BSetGetNth(alts, i);
+		sMatrix[row][col] = v + 1;
+		BSetRemove(sRows[row], v);
+		BSetRemove(sCols[col], v);
+		BSetRemove(sSqrs[sqr], v);
+
+		p = &sHistory[sCnt];
+		p->mRow    = row;
+		p->mCol    = col;
+		p->mAlts   = alts;
+		p->mAltCnt = altCnt;
+		p->mAltIdx = i;
+		++sCnt;
+	}
+
+   /*---------------------------------------------------------------------------
+   *  print complete matrix
+   */
+   for (i=0; i<cMaxNum; ++i) {
+      printf("%d %d %d %d %d %d %d %d %d\n",
+         sMatrix[i][0],
+         sMatrix[i][1],
+         sMatrix[i][2],
+         sMatrix[i][3],
+         sMatrix[i][4],
+         sMatrix[i][5],
+         sMatrix[i][6],
+         sMatrix[i][7],
+         sMatrix[i][8]);
+   }
+
+   exit(EXIT_SUCCESS);
+}
